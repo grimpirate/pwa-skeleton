488c263c41d4d795d31f3a256e84176975e5556d
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-17 18:35:40 -0400

    Switch to React from Mithril

---
+++ js/import.js
@@ -0,0 +1,3 @@
+export const e = React.createElement;
+export const Fragment = React.Fragment;
+export const createRoot = ReactDOM.createRoot;
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { createRoot, e, Fragment } from './import.js';
+
 (()=>{
 	if('serviceWorker' in navigator)
 	{
@@ -10,14 +12,14 @@
 	window.addEventListener('beforeinstallprompt', ev => {
 		ev.preventDefault();
 
-		install = m('button', {onclick: async () => {
+		install = e('button', {onClick: async () => {
 			ev.prompt();
 			await ev.userChoice;
 		}},
 			'Install',
-			m('span'),
-			m('svg', {viewBox: '0 0 10 10', role: 'img', 'aria-hidden': 'true', focusable: 'false'},
-				m('path', {d: 'M0 9H10M1 3L5 7L9 3M5 0V6'})));
+			e('span'),
+			e('svg', {viewBox: '0 0 10 10', role: 'img', 'aria-hidden': 'true', focusable: 'false'},
+				e('path', {d: 'M0 9H10M1 3L5 7L9 3M5 0V6'})));
 
 		m.redraw();
 	}, {once: true});
@@ -28,27 +30,26 @@
 		url: 'https://grimpirate.github.io/pwa-skeleton/'
 	};
 
-	m.mount(document.body, {
-		view: () => {
-			return [
-				m('header', install),
-				m('main',
-					m('custom-icon')),
-				m('footer',
-					navigator.canShare(shareData)
-					? m('button', {onclick: async () => {
-						await navigator.share(shareData)
-					}},
-						'Share',
-						m('span'),
-						m('svg', {viewBox: '0 0 10 10', role: 'img', 'aria-hidden': 'true', focusable: 'false'},
-							m('path', {d: 'M8 8L2 5L8 2'}),
-							m('circle', {cx: 8, cy: 2, r: 2}),
-							m('circle', {cx: 2, cy: 5, r: 2}),
-							m('circle', {cx: 8, cy: 8, r: 2})))
-					: null
-				)
-			];
-		}
-	});
+	function App()
+	{
+		return e(Fragment,
+			e('header', install),
+			e('main',
+				e('custom-icon')),
+			e('footer',
+				navigator.canShare(shareData)
+				? e('button', {onClick: async() => {
+					await navigator.share(shareData);
+				}},
+					'Share',
+					e('span'),
+					e('svg', {viewBox: '0 0 10 10', role: 'img', 'aria-hidden': 'true', focusable: 'false'},
+						e('path', {d: 'M8 8L2 5L8 2'}),
+						e('circle', {cx: 8, cy: 2, r: 2}),
+						e('circle', {cx: 2, cy: 5, r: 2}),
+						e('circle', {cx: 8, cy: 8, r: 2})))
+				: null));
+	}
+
+	createRoot(document.body).render(e(App));
 })();
