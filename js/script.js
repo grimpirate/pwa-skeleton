c80e467f51c5576a6c0c6d954e6cd37e747070ed
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-17 03:04:46 -0400

    Mithril

--- js/script.js
+++ js/script.js
@@ -5,35 +5,50 @@
 		navigator.serviceWorker.register('service-worker.js');
 	}
 
+	let install = null;
+
+	window.addEventListener('beforeinstallprompt', ev => {
+		ev.preventDefault();
+
+		install = m('button', {onclick: async () => {
+			ev.prompt();
+			await ev.userChoice;
+		}},
+			'Install',
+			m('span'),
+			m('svg', {viewBox: '0 0 10 10', role: 'img', 'aria-hidden': 'true', focusable: 'false'},
+				m('path', {d: 'M0 9H10M1 3L5 7L9 3M5 0V6'})));
+
+		m.redraw();
+	}, {once: true});
+
 	const shareData = {
 		title: 'PWA',
 		text: 'Progressive Web Application',
 		url: 'https://grimpirate.github.io/pwa-skeleton/'
 	};
 
-	if(navigator.canShare(shareData))
-	{
-		const share = document.querySelector('footer > button');
-
-		share.addEventListener('click', async () => {
-			await navigator.share(shareData);
-		});
-
-		share.classList.toggle('hidden');
-	}
-
-	window.addEventListener('beforeinstallprompt', ev => {
-		ev.preventDefault();
-
-		const install = document.querySelector('header > button');
-
-		install.addEventListener('click', async () => {
-			ev.prompt();
-			await ev.userChoice;
-
-			install.classList.toggle('hidden');
-		}, {once: true});
-
-		install.classList.toggle('hidden');
-	}, {once: true});
+	m.mount(document.body, {
+		view: () => {
+			return [
+				m('header', install),
+				m('main',
+					m('custom-icon')),
+				m('footer',
+					navigator.canShare(shareData)
+					? m('button', {onclick: async () => {
+						await navigator.share(shareData)
+					}},
+						'Share',
+						m('span'),
+						m('svg', {viewBox: '0 0 10 10', role: 'img', 'aria-hidden': 'true', focusable: 'false'},
+							m('path', {d: 'M8 8L2 5L8 2'}),
+							m('circle', {cx: 8, cy: 2, r: 2}),
+							m('circle', {cx: 2, cy: 5, r: 2}),
+							m('circle', {cx: 8, cy: 8, r: 2})))
+					: null
+				)
+			];
+		}
+	});
 })();
--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,12 @@
-const CACHE_VERSION = 28;
+const CACHE_VERSION = 29;
 const CURRENT_CACHE = `pwa-cache-v${CACHE_VERSION}`;
 const prefetch = [
-    '/',
-    '/index.html',
     '/js/script.js',
     '/css/style.css',
+    '/js/lib/mithril.min.js',
     '/js/elements/custom-icon.js'
 ].map(f => `/pwa-skeleton${f}`);
 
 self.addEventListener('install', ev => ev.waitUntil(self.skipWaiting().then(() => caches.open(CURRENT_CACHE).then(cache => cache.addAll(prefetch)))), {once: true});
 self.addEventListener('activate', ev => ev.waitUntil(caches.keys().then(cacheNames => cacheNames.map(cacheName => CURRENT_CACHE !== cacheName ? caches.delete(cacheName) : null))), {once: true});
-self.addEventListener(
-    'fetch',
-    ev => ev.respondWith(
-        caches.match(
-            ev.request, {
-                ignoreSearch:true,
-                cacheName: CURRENT_CACHE
-            }
-        ).then(res => {
-            if(res)
-            {
-                console.info('CACHED');
-                console.log(res);
-                return res;
-            }
-            else
-            {
-                console.info('FETCHED');
-                console.log(ev.request);
-                return fetch(ev.request);
-            }
-        })));
+self.addEventListener('fetch', ev => ev.respondWith(caches.match(ev.request, {ignoreSearch:true, cacheName: CURRENT_CACHE}).then(res => res || fetch(ev.request))));
