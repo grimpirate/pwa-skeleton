33ab9442e0ead876e443a6e400f960a783f89656
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-21 22:57:55 -0400

    import.js never cached

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
-const CACHE_VERSION = 70;
+const CACHE_VERSION = 71;
 const CURRENT_CACHE = `pwa-cache-v${CACHE_VERSION}`;
 const prefetch = [
     '',
     'index.html',
     'css/style.css',
+    'js/import.js',
     'js/script.js',
     'js/lib/react-dom.production.min.js',
     'js/lib/react.production.min.js',
@@ -16,7 +17,4 @@ const prefetch = [
 
 self.addEventListener('install', ev => ev.waitUntil(self.skipWaiting().then(() => caches.open(CURRENT_CACHE).then(cache => cache.addAll(prefetch)))), {once: true});
 self.addEventListener('activate', ev => ev.waitUntil(caches.keys().then(cacheNames => cacheNames.map(cacheName => CURRENT_CACHE !== cacheName ? caches.delete(cacheName) : null))), {once: true});
-self.addEventListener('fetch', ev => {
-    console.log(ev.request);
-    ev.respondWith(caches.match(ev.request, {ignoreSearch:true, cacheName: CURRENT_CACHE}).then(res => res || fetch(ev.request)))
-});
+self.addEventListener('fetch', ev => ev.respondWith(caches.match(ev.request, {ignoreSearch:true, cacheName: CURRENT_CACHE}).then(res => res || fetch(ev.request))));
