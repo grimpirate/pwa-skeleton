928678390f08c42f21d89f0fe84c4f783394a021
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-21 20:55:00 -0400

    service-worker

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import App from './views/App.js';
 if('serviceWorker' in navigator)
 {
 	navigator.serviceWorker.addEventListener('controllerchange', () => window.confirm("New version! OK to update?") ? window.location.reload(true) : null);
-	navigator.serviceWorker.register('service-worker.js');
+	navigator.serviceWorker.register('./service-worker.js');
 }
 
 createRoot(document.querySelector('#app')).render(e(App));
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 59;
+const CACHE_VERSION = 60;
 const CURRENT_CACHE = `pwa-cache-v${CACHE_VERSION}`;
 const prefetch = [
     '/',
