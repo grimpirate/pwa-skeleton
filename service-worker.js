33020e2b939441d9c3ed9a9f0e1eed6f48c12bbe
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-21 21:12:54 -0400

    relative urls implemented

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
-const CACHE_VERSION = 63;
+const CACHE_VERSION = 64;
 const CURRENT_CACHE = `pwa-cache-v${CACHE_VERSION}`;
 const prefetch = [
-    '/',
-    '/index.html',
-    '/js/script.js',
-    '/css/style.css',
-    '/js/lib/react-dom.production.min.js',
-    '/js/lib/react.production.min.js',
-    '/js/elements/custom-icon.js',
-    '/js/components/Install.js',
-    '/js/components/Share.js',
-    '/js/views/App.js'
-].map(f => `/pwa-skeleton${f}`);
+    '',
+    'index.html',
+    'js/script.js',
+    'css/style.css',
+    'js/lib/react-dom.production.min.js',
+    'js/lib/react.production.min.js',
+    'js/elements/custom-icon.js',
+    'js/components/Install.js',
+    'js/components/Share.js',
+    'js/views/App.js'
+].map(f => `./${f}`);
 
 self.addEventListener('install', ev => ev.waitUntil(self.skipWaiting().then(() => caches.open(CURRENT_CACHE).then(cache => cache.addAll(prefetch)))), {once: true});
 self.addEventListener('activate', ev => ev.waitUntil(caches.keys().then(cacheNames => cacheNames.map(cacheName => CURRENT_CACHE !== cacheName ? caches.delete(cacheName) : null))), {once: true});
