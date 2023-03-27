563982b79e8cd2cdac6d4f0402cc54f8cac7e566
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-27 11:50:12 -0400

    claim to trigger controllerchange

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 74;
+const CACHE_VERSION = 75;
 const CURRENT_CACHE = `pwa-cache-v${CACHE_VERSION}`;
 const prefetch = [
     '',
@@ -16,5 +16,5 @@ const prefetch = [
 ].map(f => `./${f}`);
 
 self.addEventListener('install', ev => ev.waitUntil(self.skipWaiting().then(() => caches.open(CURRENT_CACHE).then(cache => cache.addAll(prefetch)))), {once: true});
-self.addEventListener('activate', ev => ev.waitUntil(caches.keys().then(cacheNames => cacheNames.map(cacheName => CURRENT_CACHE !== cacheName ? caches.delete(cacheName) : null))), {once: true});
+self.addEventListener('activate', ev => ev.waitUntil(clients.claim().then(() => caches.keys().then(cacheNames => cacheNames.map(cacheName => CURRENT_CACHE !== cacheName ? caches.delete(cacheName) : null)))), {once: true});
 self.addEventListener('fetch', ev => ev.respondWith(caches.match(ev.request, {ignoreSearch:true, cacheName: CURRENT_CACHE}).then(res => res || fetch(ev.request))));
