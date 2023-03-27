e0c49cbb674020a4193cfc28ccde1cc2d9685a8c
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-27 13:53:24 -0400

    Service worker registration

---
+++ js/register.js
@@ -0,0 +1,23 @@
+if('serviceWorker' in navigator)
+{
+	function invokeUpdate(registration)
+	{
+		if(!registration) return;
+		window.confirm("New version! OK to update?") ? registration.postMessage('SKIP_WAITING') : null;
+	}
+
+	navigator.serviceWorker.addEventListener('controllerchange', () => window.location.reload(true));
+	
+	navigator.serviceWorker.register('./service-worker.js').then(registration => {
+		if(registration.waiting)
+			invokeUpdate(registration.waiting);
+
+		registration.addEventListener('updatefound', () => {
+			if(registration.installing)
+				registration.addEventListener('statechange', () => {
+					if(registration.waiting)
+						invokeUpdate(registration.waiting);
+				});
+		});
+	});
+}
