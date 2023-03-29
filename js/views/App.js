3456de6544ed25192cc94fddcc9cc3fb7aa6d16b
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-28 21:40:40 -0400

    Versioning

--- js/views/App.js
+++ js/views/App.js
@@ -6,7 +6,7 @@ export default function App()
 {
 	return e(Fragment, null,
 		e('header', null, e(Install)),
-		e('aside', null, 'v96'),
+		e('aside', null, 'v97'),
 		e('main', null, e('custom-icon')),
 		e('footer', null, e(Share)));
 }
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 96;
+const CACHE_VERSION = 97;
 const CURRENT_CACHE = `pwa-cache-v${CACHE_VERSION}`;
 const prefetch = [
     '',
