0deb798b434bc4d80a22d83d3f52e3d4eef806f7
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-29 02:46:29 -0400

    skipWaiting

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 101;
+const CACHE_VERSION = 102;
 const CURRENT_CACHE = `pwa-cache-v${CACHE_VERSION}`;
 const prefetch = [
     '',
@@ -15,6 +15,6 @@ const prefetch = [
     'media/vector/icon.svg'
 ].map(f => `/pwa-skeleton/${f}`);
 
-self.addEventListener('install', ev => ev.waitUntil(caches.open(CURRENT_CACHE).then(cache => cache.addAll(prefetch))), {once: true});
+self.addEventListener('install', ev => ev.waitUntil(self.skipWaiting().then(() => caches.open(CURRENT_CACHE).then(cache => cache.addAll(prefetch)))), {once: true});
 self.addEventListener('activate', ev => ev.waitUntil(caches.keys().then(cacheNames => Promise.all(cacheNames.filter(cacheName => CURRENT_CACHE !== cacheName).map(cacheName => caches.delete(cacheName))))), {once: true});
 self.addEventListener('fetch', ev => ev.respondWith(caches.match(ev.request, {ignoreSearch:true, cacheName: CURRENT_CACHE}).then(res => res || fetch(ev.request))));
