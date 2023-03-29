f0330f757c14b086bd0183db41a36f31f4f37eff
grimpirate <grimpirate_jrs@yahoo.com>
2023-03-28 21:21:24 -0400

    Versioning

--- js/views/App.js
+++ js/views/App.js
@@ -6,7 +6,7 @@ export default function App()
 {
 	return e(Fragment, null,
 		e('header', null, e(Install)),
-		e('aside', null, 'v95'),
+		e('aside', null, 'v96'),
 		e('main', null, e('custom-icon')),
 		e('footer', null, e(Share)));
 }
